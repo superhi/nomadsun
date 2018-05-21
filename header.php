659a0a168dae34363e74d9a58170ae386e883f19
Lawrence Gosset <lawrencegosset@gmail.com>
2018-05-21 21:40:17 +0100

    Revert code

--- header.php
+++ header.php
@@ -20,15 +20,6 @@
 	<link href="https://fonts.googleapis.com/css?family=Archivo+Black|Cardo:400,400i|Tenor+Sans" rel="stylesheet">
 	<link rel="stylesheet" href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"/>
 	<?php wp_head(); ?>
-
-	<style type="text/css">
-		/* just to remove the admin bar gap at the top temporarily */
-		html {
-			margin-top: 0 !important;
-		}
-	</style>
-
-
 </head>
 
 <body <?php body_class(); ?>>
@@ -36,13 +27,32 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'nomadsun' ); ?></a>
 
 	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$nomadsun_description = get_bloginfo( 'description', 'display' );
+			if ( $nomadsun_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $nomadsun_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nomadsun' ); ?></button>
 			<?php
-			// wp_nav_menu( array(
-			// 	'theme_location' => 'menu-1',
-			// 	'menu_id'        => 'primary-menu',
-			// ) );
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -23,14 +23,6 @@
 		<!-- make the text white and center it -->
 		<div class="hero-content white tc">
 
-			<!-- here we use the_permalink() template tag to link
-			to our post -->
-			<!-- <h1>
-				<a href="<?php the_permalink(); ?>" class="white link">
-					Go and see <?php the_title(); ?>
-				</a>
-			</h1> -->
-
 			<!-- our location title -->
 			<!-- here we make it bold, have 0 margin and uppercase letters -->
 			<h1 class="hero-heading archivo b mt0 mb3 ttu"><?php the_title(); ?></h1>
@@ -57,56 +49,9 @@
 		</div>
 	</section>
 
-	<?php
-		// if we have some flexible content, let’s loop through it
-		if( have_rows('content') ): while ( have_rows('content') ) : the_row();
-			// if it’s a header, go through the data
-			if( get_row_layout() == 'header' ):
-				// share data between the hero and header
-			?>
-
-			<header class="entry-header flex">
-				<!-- our header image goes in here -->
-				<!-- w-60 makes our hero image 60% width -->
-				<div class="w-60">
-					<div class="aspect-ratio--4x3 cover bg-right" style="<?php if( get_field('hero_image') ): ?>background-image: url(<?php the_field('hero_image'); ?>);<?php endif; ?>"></div>
-				</div>
-				<!-- our header content goes in here -->
-				<div class="w-40 ph4 flex items-center justify-center">
-					<div class="tc">
-						<p class="f6 archivo ma0 ttu tracked mb5">
-							<?php echo date("F Y", strtotime(get_field('date'))); ?>
-						</p>
-
-						<h1 class="f1 archivo b mt0 mb3 ttu">
-							<?php the_title(); ?>
-						</h1>
-
-						<p class="f1 tenor mb4 mt0 ttu">
-							<?php the_field('subhead'); ?>
-						</p>
-
-						<p class="cardo i ma0 lh-copy measure">
-							<?php the_sub_field('header_intro'); ?>
-						</p>
-
-					</div>
-
-				</div>
-			</header>
-
-			<?php
-			// if it’s a text component, show us the data
-			elseif( get_row_layout() == 'text_block' ): ?> 
-				<!-- the_sub_field('text_content'); -->
-
-				<div class="entry-content pv6 ph3 measure-wide center lh-copy">
-					<?php the_sub_field('text_content'); ?>
-				</div>
-
-			<?php endif;
-		endwhile; endif;
-	?> -->
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php //nomadsun_entry_footer(); ?>
